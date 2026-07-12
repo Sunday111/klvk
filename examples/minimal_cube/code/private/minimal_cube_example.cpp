5487e4bfa30dcb5d2175689d3e62ffc5cbb4f4fa
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-07-12 19:23:34 +0100

    Use GraphicsPipelineBuilder in minimal_cube example

--- examples/minimal_cube/code/private/minimal_cube_example.cpp
+++ examples/minimal_cube/code/private/minimal_cube_example.cpp
@@ -11,10 +11,10 @@
 #include "klvk/filesystem/filesystem.hpp"
 #include "klvk/math/transform.hpp"
 #include "klvk/mesh/procedural_mesh_generator.hpp"
-#include "klvk/template/on_scope_leave.hpp"
 #include "klvk/ui/simple_type_widget.hpp"
 #include "klvk/vulkan/device_context.hpp"
 #include "klvk/vulkan/gpu_buffer.hpp"
+#include "klvk/vulkan/graphics_pipeline_builder.hpp"
 #include "klvk/vulkan/vulkan_api.hpp"
 #include "klvk/window.hpp"
 
@@ -72,106 +72,16 @@ class CubeApp : public klvk::Application
                 .pPushConstantRanges = &push_constant_range,
             });
 
-        auto load_shader = [&](const char* name)
-        {
-            return context.CreateShaderModuleFromSource(GetShaderDir() / "just_color_3d" / name);
-        };
-        VkShaderModule vertex_shader = load_shader("just_color_3d.vert");
-        VkShaderModule fragment_shader = load_shader("just_color_3d.frag");
-        auto destroy_shaders = klvk::OnScopeLeave(
-            [&]
-            {
-                klvk::Vulkan::DestroyShaderModuleNE(device, vertex_shader);
-                klvk::Vulkan::DestroyShaderModuleNE(device, fragment_shader);
-            });
-
-        const std::array stages{
-            VkPipelineShaderStageCreateInfo{
-                .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-                .stage = VK_SHADER_STAGE_VERTEX_BIT,
-                .module = vertex_shader,
-                .pName = "main",
-            },
-            VkPipelineShaderStageCreateInfo{
-                .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-                .stage = VK_SHADER_STAGE_FRAGMENT_BIT,
-                .module = fragment_shader,
-                .pName = "main",
-            },
-        };
-        const VkVertexInputBindingDescription binding{
-            .binding = 0,
-            .stride = sizeof(edt::Vec3f),
-            .inputRate = VK_VERTEX_INPUT_RATE_VERTEX,
-        };
-        const VkVertexInputAttributeDescription attribute{
-            .location = 0,
-            .binding = 0,
-            .format = VK_FORMAT_R32G32B32_SFLOAT,
-        };
-        const VkPipelineVertexInputStateCreateInfo vertex_input{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
-            .vertexBindingDescriptionCount = 1,
-            .pVertexBindingDescriptions = &binding,
-            .vertexAttributeDescriptionCount = 1,
-            .pVertexAttributeDescriptions = &attribute,
-        };
-        const VkPipelineInputAssemblyStateCreateInfo input_assembly{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
-            .topology = mesh.topology,
-        };
-        const VkPipelineViewportStateCreateInfo viewport_state{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
-            .viewportCount = 1,
-            .scissorCount = 1,
-        };
-        const VkPipelineRasterizationStateCreateInfo rasterization{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
-            .polygonMode = VK_POLYGON_MODE_FILL,
-            .cullMode = VK_CULL_MODE_BACK_BIT,
-            .frontFace = VK_FRONT_FACE_CLOCKWISE,
-            .lineWidth = 1.f,
-        };
-        const VkPipelineMultisampleStateCreateInfo multisample{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
-            .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
-        };
-        const VkPipelineColorBlendAttachmentState blend_attachment{
-            .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
-                              VK_COLOR_COMPONENT_A_BIT,
-        };
-        const VkPipelineColorBlendStateCreateInfo color_blend{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
-            .attachmentCount = 1,
-            .pAttachments = &blend_attachment,
-        };
-        const std::array dynamic_states{VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR};
-        const VkPipelineDynamicStateCreateInfo dynamic_state{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
-            .dynamicStateCount = static_cast<uint32_t>(dynamic_states.size()),
-            .pDynamicStates = dynamic_states.data(),
-        };
-        const VkFormat color_format = GetSwapchainFormat();
-        const VkPipelineRenderingCreateInfo rendering_info{
-            .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO,
-            .colorAttachmentCount = 1,
-            .pColorAttachmentFormats = &color_format,
-        };
-        const VkGraphicsPipelineCreateInfo pipeline_info{
-            .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
-            .pNext = &rendering_info,
-            .stageCount = static_cast<uint32_t>(stages.size()),
-            .pStages = stages.data(),
-            .pVertexInputState = &vertex_input,
-            .pInputAssemblyState = &input_assembly,
-            .pViewportState = &viewport_state,
-            .pRasterizationState = &rasterization,
-            .pMultisampleState = &multisample,
-            .pColorBlendState = &color_blend,
-            .pDynamicState = &dynamic_state,
-            .layout = pipeline_layout_,
-        };
-        pipeline_ = klvk::Vulkan::CreateGraphicsPipelines(device, VK_NULL_HANDLE, std::span{&pipeline_info, 1}).front();
+        const std::filesystem::path shader_dir = GetShaderDir() / "just_color_3d";
+        pipeline_ = klvk::GraphicsPipelineBuilder(*this)
+                        .Layout(pipeline_layout_)
+                        .VertexShaderFile(shader_dir / "just_color_3d.vert")
+                        .FragmentShaderFile(shader_dir / "just_color_3d.frag")
+                        .Topology(mesh.topology)
+                        .CullMode(VK_CULL_MODE_BACK_BIT, VK_FRONT_FACE_CLOCKWISE)
+                        .VertexBinding(0, sizeof(edt::Vec3f), VK_VERTEX_INPUT_RATE_VERTEX)
+                        .VertexAttribute(0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0)
+                        .Build();
     }
 
     void Tick() override
