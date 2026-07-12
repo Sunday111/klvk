dbe350e12fc4ae74872e6aefea606d3473653020
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-07-12 19:34:56 +0100

    Use GraphicsPipelineBuilder in fractal CreateFullscreenPipeline
    The fullscreen helper now delegates to the builder's defaults plus
    Stages(), collapsing the hand-rolled pipeline setup.

--- examples/fractal/code/private/graphics_utils.cpp
+++ examples/fractal/code/private/graphics_utils.cpp
@@ -2,6 +2,7 @@
 
 #include "fractal_settings.hpp"
 #include "klvk/vulkan/device_context.hpp"
+#include "klvk/vulkan/graphics_pipeline_builder.hpp"
 
 // Vulkan create-info structs are designed for partial designated initialization;
 // unlisted fields must be zero.
@@ -30,71 +31,7 @@ VkPipeline CreateFullscreenPipeline(
     VkPipelineLayout pipeline_layout,
     std::span<const VkPipelineShaderStageCreateInfo> stages)
 {
-    const VkPipelineVertexInputStateCreateInfo vertex_input{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
-    };
-    const VkPipelineInputAssemblyStateCreateInfo input_assembly{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
-        .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
-    };
-    const VkPipelineViewportStateCreateInfo viewport_state{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
-        .viewportCount = 1,
-        .scissorCount = 1,
-    };
-    const VkPipelineRasterizationStateCreateInfo rasterization{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
-        .polygonMode = VK_POLYGON_MODE_FILL,
-        .cullMode = VK_CULL_MODE_NONE,
-        .frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
-        .lineWidth = 1.f,
-    };
-    const VkPipelineMultisampleStateCreateInfo multisample{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
-        .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
-    };
-    const VkPipelineColorBlendAttachmentState blend_attachment{
-        .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT |
-                          VK_COLOR_COMPONENT_A_BIT,
-    };
-    const VkPipelineColorBlendStateCreateInfo color_blend{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
-        .attachmentCount = 1,
-        .pAttachments = &blend_attachment,
-    };
-    const std::array dynamic_states{VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR};
-    const VkPipelineDynamicStateCreateInfo dynamic_state{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
-        .dynamicStateCount = static_cast<uint32_t>(dynamic_states.size()),
-        .pDynamicStates = dynamic_states.data(),
-    };
-
-    const VkFormat color_format = app.GetSwapchainFormat();
-    const VkPipelineRenderingCreateInfo rendering_info{
-        .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO,
-        .colorAttachmentCount = 1,
-        .pColorAttachmentFormats = &color_format,
-    };
-
-    const VkGraphicsPipelineCreateInfo pipeline_info{
-        .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
-        .pNext = &rendering_info,
-        .stageCount = static_cast<uint32_t>(stages.size()),
-        .pStages = stages.data(),
-        .pVertexInputState = &vertex_input,
-        .pInputAssemblyState = &input_assembly,
-        .pViewportState = &viewport_state,
-        .pRasterizationState = &rasterization,
-        .pMultisampleState = &multisample,
-        .pColorBlendState = &color_blend,
-        .pDynamicState = &dynamic_state,
-        .layout = pipeline_layout,
-    };
-    return klvk::Vulkan::CreateGraphicsPipelines(
-               app.GetDeviceContext().GetDevice(),
-               VK_NULL_HANDLE,
-               std::span{&pipeline_info, 1})
-        .front();
+    return klvk::GraphicsPipelineBuilder(app).Layout(pipeline_layout).Stages(stages).Build();
 }
 
 void CmdSetGlStyleViewport(VkCommandBuffer command_buffer, const klvk::Viewport& viewport, edt::Vec2u32 framebuffer)
