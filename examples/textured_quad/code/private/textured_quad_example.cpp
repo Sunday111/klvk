eaae155c48a9c4982b5f22c6453f462aeabb683d
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2026-07-13 11:08:31 +0100

    Use VkObject and DescriptorSets in textured_quad example

--- examples/textured_quad/code/private/textured_quad_example.cpp
+++ examples/textured_quad/code/private/textured_quad_example.cpp
@@ -4,9 +4,11 @@
 #include "klvk/error_handling.hpp"
 #include "klvk/filesystem/filesystem.hpp"
 #include "klvk/texture/procedural_texture_generator.hpp"
+#include "klvk/vulkan/descriptor_sets.hpp"
 #include "klvk/vulkan/device_context.hpp"
 #include "klvk/vulkan/graphics_pipeline_builder.hpp"
 #include "klvk/vulkan/texture.hpp"
+#include "klvk/vulkan/vk_object.hpp"
 #include "klvk/vulkan/vulkan_api.hpp"
 #include "klvk/window.hpp"
 
@@ -45,59 +47,10 @@ class TexturedQuadApp : public klvk::Application
         }
 
         // Descriptor set that binds the texture to the fragment shader
-        {
-            const VkDescriptorSetLayoutBinding binding{
-                .binding = 0,
-                .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-                .descriptorCount = 1,
-                .stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT,
-            };
-            set_layout_ = klvk::Vulkan::CreateDescriptorSetLayout(
-                device,
-                {
-                    .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
-                    .bindingCount = 1,
-                    .pBindings = &binding,
-                });
-
-            const VkDescriptorPoolSize pool_size{
-                .type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-                .descriptorCount = 1,
-            };
-            descriptor_pool_ = klvk::Vulkan::CreateDescriptorPool(
-                device,
-                {
-                    .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
-                    .maxSets = 1,
-                    .poolSizeCount = 1,
-                    .pPoolSizes = &pool_size,
-                });
-
-            descriptor_set_ = klvk::Vulkan::AllocateDescriptorSets(
-                                  device,
-                                  {
-                                      .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
-                                      .descriptorPool = descriptor_pool_,
-                                      .descriptorSetCount = 1,
-                                      .pSetLayouts = &set_layout_,
-                                  })
-                                  .front();
-
-            const VkDescriptorImageInfo image_info{
-                .sampler = texture_->GetSampler(),
-                .imageView = texture_->GetView(),
-                .imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
-            };
-            const VkWriteDescriptorSet write{
-                .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-                .dstSet = descriptor_set_,
-                .dstBinding = 0,
-                .descriptorCount = 1,
-                .descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-                .pImageInfo = &image_info,
-            };
-            klvk::Vulkan::UpdateDescriptorSets(device, std::span{&write, 1});
-        }
+        descriptor_sets_ = klvk::DescriptorSets::Builder(context)
+                               .Binding(0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT)
+                               .Build();
+        descriptor_sets_.WriteImage(0, 0, texture_->GetView(), texture_->GetSampler());
 
         // Pipeline
         {
@@ -106,23 +59,28 @@ class TexturedQuadApp : public klvk::Application
                 .offset = 0,
                 .size = sizeof(PushConstants),
             };
-            pipeline_layout_ = klvk::Vulkan::CreatePipelineLayout(
+            const VkDescriptorSetLayout set_layout = descriptor_sets_.GetLayout();
+            pipeline_layout_ = klvk::VkObject<VkPipelineLayout>{
                 device,
-                {
-                    .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
-                    .setLayoutCount = 1,
-                    .pSetLayouts = &set_layout_,
-                    .pushConstantRangeCount = 1,
-                    .pPushConstantRanges = &push_constant_range,
-                });
+                klvk::Vulkan::CreatePipelineLayout(
+                    device,
+                    {
+                        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+                        .setLayoutCount = 1,
+                        .pSetLayouts = &set_layout,
+                        .pushConstantRangeCount = 1,
+                        .pPushConstantRanges = &push_constant_range,
+                    })};
 
             const std::filesystem::path shader_dir = GetShaderDir() / "textured_quad_2d";
-            pipeline_ = klvk::GraphicsPipelineBuilder(*this)
-                            .Layout(pipeline_layout_)
-                            .VertexShaderFile(shader_dir / "textured_quad_2d.vert")
-                            .FragmentShaderFile(shader_dir / "textured_quad_2d.frag")
-                            .AlphaBlend()
-                            .Build();
+            pipeline_ = klvk::VkObject<VkPipeline>{
+                device,
+                klvk::GraphicsPipelineBuilder(*this)
+                    .Layout(pipeline_layout_)
+                    .VertexShaderFile(shader_dir / "textured_quad_2d.vert")
+                    .FragmentShaderFile(shader_dir / "textured_quad_2d.frag")
+                    .AlphaBlend()
+                    .Build()};
         }
     }
 
@@ -131,13 +89,14 @@ class TexturedQuadApp : public klvk::Application
         klvk::Application::Tick();
 
         VkCommandBuffer command_buffer = GetCurrentCommandBuffer();
+        const VkDescriptorSet descriptor_set = descriptor_sets_.Get(0);
         klvk::Vulkan::CmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline_);
         klvk::Vulkan::CmdBindDescriptorSets(
             command_buffer,
             VK_PIPELINE_BIND_POINT_GRAPHICS,
             pipeline_layout_,
             0,
-            std::span{&descriptor_set_, 1});
+            std::span{&descriptor_set, 1});
 
         const PushConstants push_constants{
             .color = edt::Math::GetRainbowColorsA(GetTimeSeconds()).Cast<float>() / 255.f,
@@ -147,28 +106,11 @@ class TexturedQuadApp : public klvk::Application
         klvk::Vulkan::CmdDraw(command_buffer, 6, 1, 0, 0);
     }
 
-public:
-    ~TexturedQuadApp() override
-    {
-        // Initialize either succeeded entirely or threw before creating the pipeline.
-        if (pipeline_ == VK_NULL_HANDLE) return;
-
-        klvk::DeviceContext& context = GetDeviceContext();
-        context.WaitIdle();
-        VkDevice device = context.GetDevice();
-        klvk::Vulkan::DestroyPipelineNE(device, pipeline_);
-        klvk::Vulkan::DestroyPipelineLayoutNE(device, pipeline_layout_);
-        klvk::Vulkan::DestroyDescriptorPoolNE(device, descriptor_pool_);
-        klvk::Vulkan::DestroyDescriptorSetLayoutNE(device, set_layout_);
-    }
-
 private:
     std::unique_ptr<klvk::Texture> texture_;
-    VkDescriptorSetLayout set_layout_ = VK_NULL_HANDLE;
-    VkDescriptorPool descriptor_pool_ = VK_NULL_HANDLE;
-    VkDescriptorSet descriptor_set_ = VK_NULL_HANDLE;
-    VkPipelineLayout pipeline_layout_ = VK_NULL_HANDLE;
-    VkPipeline pipeline_ = VK_NULL_HANDLE;
+    klvk::DescriptorSets descriptor_sets_;
+    klvk::VkObject<VkPipelineLayout> pipeline_layout_;
+    klvk::VkObject<VkPipeline> pipeline_;
 };
 
 void Main()
